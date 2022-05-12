01db699150a8ba881be2b258a97e31ced4eef3d5
longtoan99 <95176764+longtoan99@users.noreply.github.com>
2022-05-12 13:22:00 +0700

    Update rdn.sh

--- rdn.sh
+++ rdn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo > /etc/init.d/rdn
-echo e-'#!/bin/sh /etc/rc.common /n START=10 /n STOP=15 /n' >> /etc/init.d/rdn
+echo -e '#!/bin/sh /etc/rc.common /n START=10 /n STOP=15 /n' >> /etc/init.d/rdn
